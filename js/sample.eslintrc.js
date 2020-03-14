e373ea719123396a97fef2e09aad829fabf8f32d
Sophie Au <SophieAu@users.noreply.github.com>
2020-03-14 21:13:39 +0100

    Update default eslintrc

--- js/sample.eslintrc.js
+++ js/sample.eslintrc.js
@@ -1,28 +1,26 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'simple-import-sort', 'jsx-a11y'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "simple-import-sort", "jsx-a11y"],
   extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
   settings: {
-    react: {
-      version: 'detect', // Tell eslint-plugin-react to autodetect React version
-    },
+    react: { version: "detect" }
   },
   rules: {
-    'react/prop-types': 0,
-    'react/display-name': 0,
-    'simple-import-sort/sort': 2,
-    '@typescript-eslint/explicit-function-return-type': 0,
-    'react/jsx-no-literals': 2,
-  },
+    "react/prop-types": 0,
+    "react/display-name": 0,
+    "simple-import-sort/sort": 2,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "react/jsx-no-literals": 2,
+    "@typescript-eslint/no-unused-vars": [2, { varsIgnorePattern: "^_$" }]
+  }
 };
-
