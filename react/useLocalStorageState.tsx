dc2a10e57973b004d800d909ff7e7ad7b0c7e8b8
Sophie Au <SophieAu@users.noreply.github.com>
2020-04-24 22:00:06 +0200

    Add useLocalStorage hook

---
+++ react/useLocalStorageState.tsx
@@ -0,0 +1,13 @@
+// taken from Kent C. Dodds' Beginner's Guide to React (https://egghead.io/lessons/react-create-reusable-custom-hooks)
+
+import { useEffect, useState } from 'react';
+
+export const useLocalStorageState = (key: string, defaultValue = '') => {
+  const [state, setState] = useState(() => window.localStorage.getItem(key) || defaultValue);
+
+  useEffect(() => {
+    window.localStorage.setItem(key, state);
+  }, [key, state]);
+
+  return [state, setState];
+};
