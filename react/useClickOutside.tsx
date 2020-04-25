afeb19865e5bfea0df82a7d44bcdef957d4fbd37
Sophie Au <SophieAu@users.noreply.github.com>
2020-04-25 19:06:16 +0200

    Add useClickOutside hook

---
+++ react/useClickOutside.tsx
@@ -0,0 +1,25 @@
+// taken from Tanner Linsley's talk at JSConf Hawaii (https://www.youtube.com/watch?v=J-g9ZJha8FE)
+
+import React, { useEffect, useRef } from "react";
+
+export const useClickOutside = (
+  elementRef: React.RefObject<HTMLElement>,
+  callback: (e: MouseEvent) => void
+) => {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        elementRef?.current?.contains(e.target as Node) &&
+        callbackRef.current
+      )
+        callbackRef.current(e);
+    };
+
+    document.addEventListener("click", handleClickOutside, true);
+
+    return () =>
+      document.removeEventListener("click", handleClickOutside, true);
+  }, [callbackRef, elementRef]);
+};
