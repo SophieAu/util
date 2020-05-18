d93a8fb1f38eb49612a75b3a5691b7b72d2bf246
Sophie Au <SophieAu@users.noreply.github.com>
2020-05-18 17:37:04 +0200

    (hopefully) check for existence of window object

--- react/useLocalStorageState.tsx
+++ react/useLocalStorageState.tsx
@@ -1,12 +1,14 @@
 // taken from Kent C. Dodds' Beginner's Guide to React (https://egghead.io/lessons/react-create-reusable-custom-hooks)
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-export const useLocalStorageState = (key: string, defaultValue = '') => {
-  const [state, setState] = useState(() => window.localStorage.getItem(key) || defaultValue);
+export const useLocalStorageState = (key: string, defaultValue = "") => {
+  const [state, setState] = useState(
+    () => window?.localStorage.getItem(key) || defaultValue
+  );
 
   useEffect(() => {
-    window.localStorage.setItem(key, state);
+    window?.localStorage.setItem(key, state);
   }, [key, state]);
 
   return [state, setState];
